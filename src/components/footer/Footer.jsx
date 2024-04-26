2ea16f1109c80d2754c99bb17df16d645b543ee3
Luis Pomar <luis.pomar.petin@gmail.com>
2024-04-26 21:55:34 +0200

    estilos generales añadidos pt.1

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.scss'
+import { NavLink } from 'react-router-dom'
+
+export default function Footer() {
+  return (
+    <>
+        <footer className='footer container bg-black'>
+            {/* <NavLink to='/languages' className='footer_link'>Languajes</NavLink>
+            <NavLink to='/families' className='footer_link'>Families</NavLink>
+            <NavLink to='/chronogram' className='footer_link'>Chronogram</NavLink> */}
+        </footer>
+    </>
+  )
+}
