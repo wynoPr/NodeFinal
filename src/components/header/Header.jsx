903de3739aa402bb42ec9e660bf45d39968fd3f3
Luis Pomar <luis.pomar.petin@gmail.com>
2024-04-28 21:36:59 +0200

    Timeline finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.scss'
 import '../../../src/App.scss'
 import { Link } from 'react-router-dom';
-import SearchBar from './../searchbar/SearchBar.jsx';
+import Finder from '../searchbar/Finder';
 
 export default function Header(location) {
 
@@ -15,8 +15,8 @@ const gobacks = () => {
   return (
     <>
       <header className='header'>
-        {path === "/languages" && <SearchBar/>}
-        {path.includes("/profile") && <Link to="javascript:void(0)" onClick={gobacks} className="link icon h3 goback"> &#60; Go Back</Link>}
+        {path === "/languages" || path === "/families"  && <Finder/>}
+        {path.includes("/profile") && <Link to="" onClick={gobacks} className="link icon h3 goback"> &#60; Go Back</Link>}
         <div className='header_language'>
           <a><img className="header_language_icons link icon" src='src\img\spain.png' alt='cambiar idioma a español '></img></a>
           <a><img className="header_language_icons link icon" src='src\img\united-kingdom.png' alt='change language to english'></img></a>
