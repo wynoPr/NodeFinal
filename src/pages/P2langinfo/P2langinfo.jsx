c5aa7378f64e6a5d49e27608239b8bf06080ffce
Luis Pomar <luis.pomar.petin@gmail.com>
2024-05-09 14:43:50 +0200

    async corrections

--- src/pages/P2langinfo/P2langinfo.jsx
+++ src/pages/P2langinfo/P2langinfo.jsx
@@ -10,6 +10,7 @@ export default function P2langinfo({ dataType }) {
   const url = "http://localhost:3001/language/" + id;
 
   useEffect(() => {
+
     const fetchData = async () => {
       try {
         const { data } = await axios(url);
@@ -19,29 +20,36 @@ export default function P2langinfo({ dataType }) {
       }
     };
     fetchData();
+
   }, []);
+
   const dFamily = data.family;
   const dFriendly = data.friendly;
-  console.log(dFriendly);
+  console.log(dFamily);
+
   return (
     <>
       <Header />
       <div className="f-w container">
         <div>
           <img src={data.img} alt={data.name} />
-          <div>
-            <p>{dFamily.name}</p>
-            <img src={dFamily.img} alt={dFamily.name} />
-          </div>
+          {dFamily && (
+            <div>
+              <p>{dFamily.name}</p>
+              <img src={dFamily.img} alt={dFamily.name} />
+            </div>
+          )}
         </div>
         <div className="h1">{data.version}</div>
-        {/* <div>
-          {dFriendly.map((friendlyItem, id) => (
-            <div key={id}>
-              <p>hola</p>
-            </div>
-          ))}
-        </div> */}
+        {dFriendly && (
+          <div>
+            {dFriendly.map((friendlyItem, id) => (
+              <div key={id}>
+                <p className="h3">hola</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <Footer />
     </>
